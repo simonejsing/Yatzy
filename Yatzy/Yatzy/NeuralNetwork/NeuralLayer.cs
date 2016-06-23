14cce9f8e8dc6749bd4c933653b31fd32c07aa12
simej <simej@microsoft.com>
2016-06-22 20:02:47 -0700

    Created base structure for neural network

---
+++ Yatzy/Yatzy/NeuralNetwork/NeuralLayer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace MachineLearning
+{
+    internal class NeuralLayer
+    {
+        public int Size { get; private set; }
+
+        private double[] nodes;
+        private double[] inputWeights;
+        private double[] outputWeights;
+
+        public NeuralLayer(int numberOfNeurons)
+        {
+            Size = numberOfNeurons;
+
+            nodes = new double[Size];
+            inputWeights = new double[Size];
+            outputWeights = new double[Size];
+
+            for (int i = 0; i < Size; i++)
+            {
+                nodes[i] = 0.0;
+                inputWeights[i] = 0.0;
+                outputWeights[i] = 0.0;
+            }
+        }
+
+        public NeuralLayer(NeuralLayer layer) : this(layer.Size)
+        {
+            for (int i = 0; i < Size; i++)
+            {
+                nodes[i] = layer.nodes[i];
+                inputWeights[i] = layer.inputWeights[i];
+                outputWeights[i] = layer.outputWeights[i];
+            }
+        }
+
+        private static double ActivationFunction(double input)
+        {
+            return Math.Max(0.5, input);
+        }
+
+        private static double DerivedActivationFunction(double input)
+        {
+            return input < 0.5 ? 0 : 1;
+        }
+    }
+}
---
+++ Yatzy/Yatzy/NeuralNetwork/NeuralNetwork.cs
@@ -0,0 +1,24 @@
+﻿namespace MachineLearning
+{
+    public class NeuralNetwork
+    {
+        private readonly NeuralLayer[] layers;
+
+        public NeuralNetwork(NeuralNetwork network) : this(network.layers.Length)
+        {
+            for (int i = 0; i < network.layers.Length; i++)
+            {
+                layers[i] = new NeuralLayer(network.layers[i]);
+            }
+        }
+
+        public NeuralNetwork(int numberOfLayers)
+        {
+            layers = new NeuralLayer[numberOfLayers];
+            for (int i = 0; i < numberOfLayers; i++)
+            {
+                layers[i] = new NeuralLayer(2);
+            }
+        }
+    }
+}
